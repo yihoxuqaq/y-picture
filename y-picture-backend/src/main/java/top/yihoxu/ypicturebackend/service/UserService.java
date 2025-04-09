89a1a28c9b0db53b2b721cac87d0ee47c77e6e8f
yihoxu <2207039249@qq.com>
2025-04-09 13:10:15 +0800

    项目初始化

---
+++ y-picture-backend/src/main/java/top/yihoxu/ypicturebackend/service/UserService.java
@@ -0,0 +1,13 @@
+package top.yihoxu.ypicturebackend.service;
+
+import top.yihoxu.ypicturebackend.pojo.User;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+* @author hushi
+* @description 针对表【user(用户)】的数据库操作Service
+* @createDate 2025-04-09 12:48:32
+*/
+public interface UserService extends IService<User> {
+
+}
